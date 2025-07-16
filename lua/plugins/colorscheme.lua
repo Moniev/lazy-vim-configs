6fce8c15b1dff749af0a3c16c1a64344272b920b
Moniev <m0ni3v@gmail.com>
2025-07-16 21:35:38 +0200

    lazy vim config

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,28 @@
+return {
+  {
+    "Mofiqul/vscode.nvim",
+    lazy = true,
+    opts = {
+      transparent = false,
+      italic_comments = true,
+      disable_nvimtree_bg = true,
+      color_overrides = {
+        vscLineNumber = "#888888",
+      },
+      group_overrides = {
+        Cursor = { fg = "#000000", bg = "#0FE1EE", bold = false },
+      },
+    },
+  },
+  {
+    "folke/tokyonight.nvim",
+    lazy = true,
+    opts = { style = "moon" },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "vscode",
+    },
+  },
+}
