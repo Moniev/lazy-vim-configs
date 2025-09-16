ab65a2f04e31acff7ac25c9d4a8d5f278c72bea6
Robert Moń <m0ni3v@gmail.com>
2025-09-16 02:46:17 +0200

    added autocmds for navigation, autolsp so on

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,8 +1,27 @@
--- Autocmds are automatically loaded on the VeryLazy event
--- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
---
--- Add any additional autocmds here
--- with `vim.api.nvim_create_autocmd`
---
--- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
--- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+vim.api.nvim_create_autocmd("BufWritePre", {
+  group = augroup,
+  callback = function()
+    vim.lsp.buf.format({ async = false, timeout_ms = 1000 })
+  end,
+})
+
+local change_directory_group = vim.api.nvim_create_augroup("ChangeDirectory", {})
+vim.api.nvim_create_autocmd("BufEnter", {
+  group = change_directory_group,
+  callback = function()
+    local file_path = vim.fn.expand("%:p:h")
+    if file_path ~= "" then
+      vim.cmd("cd " .. file_path)
+    end
+  end,
+})
+
+local trim_whitespaces = vim.api.nvim_create_augroup("TrimWhitespace", { clear = true })
+vim.api.nvim_create_autocmd("BufWritePre", {
+  group = trim_whitespaces,
+  pattern = "*",
+  callback = function()
+    vim.cmd("%s/\\s\\+$//e")
+  end,
+})
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,7 +1,19 @@
 local opts = { noremap = true, silent = true }
 
+vim.g.mapleader = " "
+
 vim.keymap.set("n", "<Tab>", ">>", opts)
 vim.keymap.set("n", "<S-Tab>", "<<", opts)
 vim.keymap.set("v", "<Tab>", ">gv", opts)
 vim.keymap.set("v", "<S-Tab>", "<gv", opts)
-vim.keymap.set("i", "<S-Tab>", "<C-\\><C-N><<<C-\\><C-N>^i")
+vim.keymap.set("i", "<S-Tab>", "<C-d>", opts)
+
+vim.keymap.set("n", "<leader>a", "ggVG", opts)
+
+vim.keymap.set("n", "<leader>d", "<cmd>lua vim.lsp.buf.definition()<cr>", { desc = "Go to definition" })
+vim.keymap.set("n", "<leader>r", "<cmd>lua vim.lsp.buf.references()<cr>", { desc = "Find references" })
+vim.keymap.set("n", "<leader>h", "<cmd>lua vim.lsp.buf.hover()<cr>", { desc = "Show hover info" })
+vim.keymap.set("n", "<leader>D", "<cmd>lua vim.lsp.buf.declaration()<cr>", { desc = "Go to declaration" })
+
+vim.keymap.set("n", "<C-s>", ":w<cr>", { desc = "Save file" })
+vim.keymap.set("n", "<leader>q", ":q<cr>", { desc = "Close buffer" })
