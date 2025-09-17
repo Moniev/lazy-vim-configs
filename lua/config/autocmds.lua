9530857d6627b5084e201db556a640d2a7d52544
Robert Moń <m0ni3v@gmail.com>
2025-09-17 20:49:42 +0200

    more flexible confs

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,35 +1,36 @@
 local my_autocmds = vim.api.nvim_create_augroup("MyAutocmds", { clear = true })
 
-vim.api.nvim_create_autocmd("BufWritePre", {
-  group = my_autocmds,
-  callback = function()
-    vim.lsp.buf.format({ async = false, timeout_ms = 1000 })
-  end,
-})
+local autocmds = {
+  {
+    event = "BufWritePre",
+    callback = function()
+      vim.lsp.buf.format({ async = false, timeout_ms = 1000 })
+    end,
+    desc = "Format file before saving",
+  },
+  {
+    event = "BufWritePre",
+    callback = function()
+      vim.cmd("%s/\\s\\+$//e")
+    end,
+    desc = "Trim trailing whitespace on save",
+  },
+  {
+    event = { "CursorHold", "CursorHoldI" },
+    callback = function()
+      local f = vim.fn.filereadable(vim.api.nvim_buf_get_name(0))
+      if f == 1 then
+        vim.cmd("checktime")
+      end
+    end,
+    desc = "Check for file changes on disk",
+  },
+}
 
-vim.api.nvim_create_autocmd("BufWritePre", {
-  group = my_autocmds,
-  callback = function()
-    vim.cmd("%s/\\s\\+$//e")
-  end,
-})
-
-vim.api.nvim_create_autocmd("BufEnter", {
-  group = my_autocmds,
-  callback = function()
-    local file_path = vim.fn.expand("%:p:h")
-    if file_path ~= "" then
-      vim.cmd("cd " .. file_path)
-    end
-  end,
-})
-
-vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
-  group = my_autocmds,
-  callback = function()
-    local f = vim.fn.filereadable(vim.api.nvim_buf_get_name(0))
-    if f == 1 then
-      vim.cmd("checktime")
-    end
-  end,
-})
+for _, autocmd in ipairs(autocmds) do
+  vim.api.nvim_create_autocmd(autocmd.event, {
+    group = my_autocmds,
+    callback = autocmd.callback,
+    desc = autocmd.desc,
+  })
+end
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,26 +1,40 @@
 local opts = { noremap = true, silent = true }
-
+local map = vim.keymap.set
 vim.g.mapleader = " "
 
-vim.keymap.set("n", "<Tab>", ">>", opts)
-vim.keymap.set("n", "<S-Tab>", "<<", opts)
-vim.keymap.set("v", "<Tab>", ">gv", opts)
-vim.keymap.set("v", "<S-Tab>", "<gv", opts)
-vim.keymap.set("i", "<S-Tab>", "<C-d>", opts)
+local keymaps = {
+  n = {
+    { "<Tab>", ">>", opts },
+    { "<S-Tab>", "<<", opts },
+    { "<leader>a", "ggVG", opts },
+    { "<leader>d", "<cmd>lua vim.lsp.buf.definition()<cr>", { desc = "Go to definition" } },
+    { "<leader>r", "<cmd>lua vim.lsp.buf.references()<cr>", { desc = "Find references" } },
+    { "<leader>h", "<cmd>lua vim.lsp.buf.hover()<cr>", { desc = "Show hover info" } },
+    { "<leader>D", "<cmd>lua vim.lsp.buf.declaration()<cr>", { desc = "Go to declaration" } },
+    { "<C-s>", ":w<cr>", { desc = "Save file" } },
+    { "<leader>q", ":q<cr>", { desc = "Close buffer" } },
+  },
 
-vim.keymap.set("n", "<leader>a", "ggVG", opts)
+  v = {
+    { "<Tab>", ">gv", opts },
+    { "<S-Tab>", "<gv", opts },
+  },
 
-vim.keymap.set("n", "<leader>d", "<cmd>lua vim.lsp.buf.definition()<cr>", { desc = "Go to definition" })
-vim.keymap.set("n", "<leader>r", "<cmd>lua vim.lsp.buf.references()<cr>", { desc = "Find references" })
-vim.keymap.set("n", "<leader>h", "<cmd>lua vim.lsp.buf.hover()<cr>", { desc = "Show hover info" })
-vim.keymap.set("n", "<leader>D", "<cmd>lua vim.lsp.buf.declaration()<cr>", { desc = "Go to declaration" })
+  i = {
+    { "<S-Tab>", "<C-d>", opts },
+  },
+}
 
-vim.keymap.set("n", "<C-s>", ":w<cr>", { desc = "Save file" })
-vim.keymap.set("n", "<leader>q", ":q<cr>", { desc = "Close buffer" })
-
-vim.keymap.set("n", "<leader>j", function()
-  local line_number = vim.fn.input("type number of line: ")
-  if line_number and tonumber(line_number) then
-    vim.cmd(line_number)
+for mode, maps in pairs(keymaps) do
+  for _, mapping in ipairs(maps) do
+    if #mapping == 3 then
+      map(mode, mapping[1], mapping[2], mapping[3])
+    else
+      local key = mapping[1]
+      local rhs = mapping[2]
+      local custom_opts = mapping[3]
+      local final_opts = vim.tbl_deep_extend("force", {}, opts, custom_opts)
+      map(mode, key, rhs, final_opts)
+    end
   end
-end, { noremap = true, silent = true, desc = "Jump for inserted line" })
+end
