5ea7486885f8fc1b3276d1d3888412282117cdb1
moniev <m0ni3v@gmail.com>
2025-11-29 02:25:48 +0100

    feat: Add git checkout and git branch keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -39,20 +39,31 @@ local keymaps = {
     { "<A-Up>", "<C-w>k", { desc = "Go to window above" } },
     { "<A-Right>", "<C-w>l", { desc = "Go to right window" } },
 
+    {
+      "<leader>gb",
+      function()
+        vim.ui.input({ prompt = "New branch name: " }, function(branch)
+          if branch and branch ~= "" then
+            vim.cmd("G checkout -b " .. branch)
+          end
+        end)
+      end,
+      { desc = "Git create new branch" },
+    },
     { "<leader>gs", "<cmd>G<cr>", { desc = "Git status" } },
     { "<leader>gd", "<cmd>Gdiffsplit<cr>", { desc = "Git diff" } },
     { "<leader>ga", "<cmd>Gwrite<cr>", { desc = "Git add (current buffer)" } },
     { "<leader>gc", "<cmd>G commit<cr>", { desc = "Git commit" } },
     { "<leader>gA", "<cmd>G add --all<cr>", { desc = "Git add all" } },
     { "<leader>gr", "<cmd>GDelete<cr>", { desc = "Git remove (current file)" } },
-    { "<leader>gM", "<cmd>G switch main <cr>", { desc = "Git switch main" } },
+    { "<leader>gM", "<cmd>G switch main<cr>", { desc = "Git switch main" } },
     { "<leader>gRm", "<cmd>G rebase origin/main <cr>", { desc = "Git rebase main" } },
     { "<leader>gU", "<cmd>G add -u<cr>", { desc = "Git add -u" } },
     { "<leader>gu", "<cmd>G reset HEAD %<cr>", { desc = "Git unstage (current file)" } },
     { "<leader>gP", "<cmd>G push<cr>", { desc = "Git push" } },
     { "<leader>gPP", "<cmd>G push --force-with-lease<cr>", { desc = "Git push force with lease" } },
     { "<leader>gPPP", "<cmd>G push --force<cr>", { desc = "Git push with force`" } },
-
+    { "<leader>go", ":G checkout ", { desc = "Git checkout (existing)" } },
     { "<leader>gL", "<cmd>G pull<cr>", { desc = "Git pull" } },
     { "<leader>gS", "<cmd>G stash<cr>", { desc = "Git stash" } },
     { "<leader>gsp", "<cmd>G stash pop<cr>", { desc = "Git stash pop" } },
